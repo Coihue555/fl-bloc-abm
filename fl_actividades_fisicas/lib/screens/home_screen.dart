168bc83c9e8a20adef83febaf980cbc45a9d9942
Coihue555 <kevlar555@gmail.com>
2022-03-24 15:36:21 -0300

    Arreglado detalles visuales

--- fl_actividades_fisicas/lib/screens/home_screen.dart
+++ fl_actividades_fisicas/lib/screens/home_screen.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:fl_actividades_fisicas/widgets/listado_actividades.dart';
 
-
 class HomeScreen extends StatelessWidget {
-   
   const HomeScreen({Key? key}) : super(key: key);
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Lista de Actividades'),
       ),
-      body: const Center(
-         child: ActividadesListTiles(),
-      ),
+      body: Container(
+          padding: const EdgeInsets.only(bottom: 20, right: 5),
+          child: const ActividadesListTiles()),
     );
   }
 }
